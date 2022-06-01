5bab434232fa8399fbcb1fe383c9baf207cdbc50
Roman <rkanafonskyi@opengov.com>
2022-06-01 21:35:09 +0300

    added user + prettier

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable,
+         :trackable
+
+  has_many :posts
+
+  # TODO: add validations
+  # TODO: add callback to set last_seen_at
 end
