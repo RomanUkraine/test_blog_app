841d9d39b1d2acbdcad2866676154f135830c579
Roman <rkanafonskyi@opengov.com>
2022-06-06 15:14:44 +0300

    updated nav and login form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |u|
-      u.permit(:first_name, :last_name, :email, :password)
+      u.permit(:first_name, :last_name, :email, :password, :password_confirmation)
     end
     devise_parameter_sanitizer.permit(:account_update) do |u|
       u.permit(:first_name, :last_name, :email, :password, :current_password)
